d6921da7e02fbf9038286ad5274e0c95f9f4eb33
DeathMasterSM <deathmastersm@gmail.com>
2020-02-16 20:44:56 +0300

    Changes1

--- Дз от 16.02.2020/Source.cpp
+++ Дз от 16.02.2020/Source.cpp
@@ -6,9 +6,22 @@ T sum(T a, T b)
 	return (a + b);
 };
 
+class test_status
+{
+public:
+	test_status(bool a, int b)
+	{
+		completed_or_not = a;
+		mark = b;
+	}
+	bool completed_or_not;
+	int mark;
+};
+
 int main()
 {
-	int a = 5;
+	test_status Student1(true, 3);
+	int a = 5; 
 	int b = 6;
 	float c = 2.00;
 	float d = 5.69;
